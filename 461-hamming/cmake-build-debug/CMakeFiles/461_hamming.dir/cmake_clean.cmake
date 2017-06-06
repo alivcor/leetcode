cc6785f09b40cbdc96997410aeed3c69c0f827dc
alivcor <abhinandandubey@live.com>
2017-06-06 16:05:48 -0400

    added 561

---
+++ 461-hamming/cmake-build-debug/CMakeFiles/461_hamming.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/461_hamming.dir/main.cpp.o"
+  "461_hamming.pdb"
+  "461_hamming"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/461_hamming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
