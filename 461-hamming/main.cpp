0e4f161b30655d78eccd75e324af0c3a05be9516
alivcor <abhinandandubey@live.com>
2017-06-06 14:23:40 -0400

    checking out

--- 461-hamming/main.cpp
+++ 461-hamming/main.cpp
@@ -6,24 +6,47 @@ class Solution {
 public:
     std::vector<bool> dec2bin(int num){
         std::vector<bool> bin_num;
-        while(num!=0){
-            bool rem = num%2;
+        bool rem;
+        while(num!=0 && (num!=1 || rem!=1)){
+            std::cout << " \n";
+            for(int i = 0; i < bin_num.size(); i++){
+                std::cout << bin_num[i];
+            }
+            rem = num%2;
             num /= 2;
+            std::cout << "**";
             bin_num.insert(bin_num.begin(), rem);
+            for(int i = 0; i < bin_num.size(); i++){
+                std::cout << bin_num[i];
+            }
         };
-        std::copy(bin_num.begin(), bin_num.end(), std::ostream_iterator<char>(std::cout, " "));
+//        for (auto i = bin_num.begin(); i != bin_num.end(); ++i)
+//            std::cout << *i;
+        std::cout << " \n";
+        for(int i = 0; i < bin_num.size(); i++){
+            std::cout << bin_num[i];
+        }
 
         return bin_num;
     }
+    std::string toBinary(int n)
+    {
+        std::string r;
+        while(n!=0) {r=(n%2==0 ?"0":"1")+r; n/=2;}
+        return r;
+    }
     int hammingDistance(int x, int y) {
-        std::vector<bool> bx = dec2bin(x);
-        std::vector<bool> by = dec2bin(y);
+        std::string bx = toBinary(x);
+        std::cout << bx;
+        std::cout << "\n ---- \n";
+        std::string by = toBinary(y);
+        std::cout << by;
     }
 };
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     Solution sol;
-    std::cout << sol.hammingDistance(101,1002);
+    std::cout << sol.hammingDistance(101,4);
     return 0;
 }
