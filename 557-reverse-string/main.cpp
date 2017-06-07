0cff7d75dd9dca83f61574fc4f901194f5b4f5d4
alivcor <abhinandandubey@live.com>
2017-06-06 21:35:56 -0400

    cpp sol complete !

--- 557-reverse-string/main.cpp
+++ 557-reverse-string/main.cpp
@@ -6,12 +6,18 @@ using namespace std;
 class Solution {
 public:
     string reverseWords(string s) {
-        cout<< s;
-
+        size_t front = 0;
+        for(int i = 0; i<=s.length(); i++){
+            if(s[i] == ' ' || i == s.length()){
+                reverse(&s[front], &s[i]); // reverse the last traversed word
+                front = i + 1; // 1 is for space
+            }
+        }
+        return s;
     }
 };
 int main() {
     Solution sol;
-    sol.reverseWords(std::string ("Abhinandan Dubey"));
+    cout << sol.reverseWords(std::string ("Abhinandan Dubey"));
     return 0;
 }
