7ca218a13109fd7a2d5aaaec80631fee825891af
alivcor <abhinandandubey@live.com>
2017-06-06 20:55:46 -0400

    c++ solution isn't complete

---
+++ 557-reverse-string/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        cout<< s;
+
+    }
+};
+int main() {
+    Solution sol;
+    sol.reverseWords(std::string ("Abhinandan Dubey"));
+    return 0;
+}
