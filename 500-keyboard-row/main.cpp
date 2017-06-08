874c8eba696ebd165ca3379e94fe4acf598e4858
alivcor <abhinandandubey@live.com>
2017-06-08 17:01:44 -0400

    initial

---
+++ 500-keyboard-row/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "vector"
+#include "string"
+
+class Solution {
+public:
+    vector<string> findWords(vector<string>& words) {
+
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
