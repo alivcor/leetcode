0b059b2c2f82c16cf536f8dd6b8d3f56903e5ea2
alivcor <abhinandandubey@live.com>
2017-06-12 19:22:15 -0400

    unopt

---
+++ 575-distribute-candies/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "map"
+#include "vector"
+#include "unordered_set"
+using namespace std;
+
+class Solution {
+public:
+    int distributeCandies(vector<int>& candies) {
+        int numCandies = candies.size();
+        std::map<int, int> candiesMap;
+        for(auto it=candies.begin();it!=candies.end(); ++it) {
+            if (candiesMap.find(*it) != candiesMap.end()) {
+                candiesMap[*it] = 1;
+            } else {
+                candiesMap[*it] += 1;
+            }
+        };
+        return min(numCandies/2, (int)candiesMap.size());
+
+    }
+
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    Solution sol;
+    vector<int> vector1{1,1,2,3};
+    cout << sol.distributeCandies(vector1);
+    return 0;
+}
