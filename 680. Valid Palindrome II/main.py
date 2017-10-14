c5b84b38f453791a626a8c85435673767897fca3
alivcor <abhinandandubey@live.com>
2017-10-13 22:48:57 -0400

    works

--- 680. Valid Palindrome II/main.py
+++ 680. Valid Palindrome II/main.py
@@ -1,4 +1,4 @@
-def validPalindrome(s):
+def validPalindrome_slow(s):
     """
     :type s: str
     :rtype: bool
@@ -8,5 +8,15 @@ def validPalindrome(s):
         r = s[0:i] + s[i + 1:]
         if (r == r[::-1]): return True
     return False
+
+
+def validPalindrome(s):
+    def ispalin(i,j):
+        return all(s[k]==s[j-k+i] for k in range(i,j))
+
+    for i in xrange(0,len(s)/2):
+        if(s[i] != s[len(s)-1-i]):
+            j = len(s)-1-i
+            return ispalin(i+1,j) or ispalin(i,j-1)
 
 print validPalindrome("acbdvbca")
