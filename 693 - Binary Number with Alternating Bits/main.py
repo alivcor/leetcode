06c7cf871312a58cca12b25469189cb55ac51648
alivcor <abhinandandubey@live.com>
2018-01-22 17:02:39 +0530

    update

--- 693 - Binary Number with Alternating Bits/main.py
+++
@@ -1,9 +0,0 @@
-def hasAlternatingBits(n):
-    """
-    :type n: int
-    :rtype: bool
-    """
-    m = bin(n)
-    return not any([m[i] == m[i+1] for i in range(len(m)-1)])
-
-print hasAlternatingBits(4)
