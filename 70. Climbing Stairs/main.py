7e368a01a7706dc102601ea1b2d29b35ed29147e
alivcor <abhinandandubey@live.com>
2018-01-24 14:53:11 +0530

    climbstairs optimized

--- 70. Climbing Stairs/main.py
+++ 70. Climbing Stairs/main.py
@@ -20,6 +20,28 @@ def climbStairsRecursionWithMemory(s, n, mem):
         mem[s] = climbStairsRecursionWithMemory(s+1, n, mem) + climbStairsRecursionWithMemory(s+2, n, mem)
         return mem[s]
 
+def climbStairsDPDict(n):
+    if(n==1):
+        return 1
+    dp = dict()
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]
+
+
+def climbStairsDPList(n):
+    if(n==1):
+        return 1
+    dp = [0]*(n+1)
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]
+
+
 def climbStairs(n, method):
     """
     :type n: int
@@ -30,13 +52,28 @@ def climbStairs(n, method):
     if method == 2:
         mem = dict()
         return climbStairsRecursionWithMemory(0, n, mem)
+    if method == 3:
+        return climbStairsDPList(n)
+    if method == 4:
+        return climbStairsDPDict(n)
 
 
 
+
+n = 12
 start = time()
-print climbStairs(3, 1), " | ",
+print climbStairs(n, 1), " | ",
 print time() - start
 
 start = time()
-print climbStairs(3, 2), " | ",
+print climbStairs(n, 2), " | ",
+print time() - start
+
+start = time()
+print climbStairs(n, 3), " | ",
+print time() - start
+
+
+start = time()
+print climbStairs(n, 4), " | ",
 print time() - start
