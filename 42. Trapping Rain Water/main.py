65ca23f0e9bb71597565679cf6a613329bd9bd73
alivcor <abhinandandubey@live.com>
2018-01-24 16:51:19 +0530

    trap hard TLE

--- 42. Trapping Rain Water/main.py
+++ 42. Trapping Rain Water/main.py
@@ -1,26 +1,22 @@
+
+
 def trap(heights):
     """
     :type height: List[int]
     :rtype: int
     """
-    last_big_bar = 0
-    fill_flag = False
-    big_count = 0
-    small_count = 0
-    for curr_bar in heights:
-        if fill_flag:
-            if(curr_bar <= last_big_bar):
-                small_count = small_count + last_big_bar - curr_bar
-            else:
-                # curr_bar is higher than last_big_bar
-                last_big_bar = curr_bar
-                fill_flag = False
-        else:
-            if curr_bar <= last_big_bar:
-                fill_flag = True
-            else:
-                fill_flag = False
-    return small_count
+    count = 0
+    lenh = len(heights)
+    for x in xrange(lenh):
+        curr = heights[x]
+        maxLeft = max(heights[0:x]) if x != 0 else 0
+        maxRight = max(heights[x+1:]) if x != lenh-1 else 0
+
+        canhold = min(maxLeft, maxRight) - curr
+        # print canhold
+        if canhold > 0:
+            count += canhold
+    return count
 
 
 
